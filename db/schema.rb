8e91c2215ded814cc41a31fffb83c0fb228aaa26
isaac benlfakih <isaacbenlfakih@gmail.com>
2024-11-30 00:40:24 +0100

    formatting and design

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_28_232150) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.float "price"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
