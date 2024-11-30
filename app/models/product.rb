495e512df299e4f770dca7657b2f6c6044f6e149
isaac benlfakih <isaacbenlfakih@gmail.com>
2024-11-30 12:25:38 +0100

    cloudinary/show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :product_orders
   has_many :orders, through: :product_orders
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,32 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Product.create(name: "Body Cream", description: "This is body cream", price: 11.30)
-Product.create(name: "Face Cream", description: "This is body cream", price: 14.30)
-Product.create(name: "Foot Cream", description: "This is body cream", price: 15.30)
-Product.create(name: "Knee Cream", description: "This is body cream", price: 12.30)
-Product.create(name: "Leg Cream", description: "This is body cream", price: 16.30)
-Product.create(name: "Elbow Cream", description: "This is body cream", price: 17.30)
+product = Product.create(name: "Majorille Garden Arganolie", description: "This is body cream", price: 24.95)
+product.photo.attach(io: File.open("/home/isaacbenlfakih/code/IsaacBenlfakih/MajorilleGarden/app/assets/images/P3.jpg"), filename: "P3.jpg", content_type: "image/png")
+product.save
+
+product = Product.create(name: "Majorille Garden Arganolie", description: "This is body cream", price: 13.00)
+product.photo.attach(io: File.open("/home/isaacbenlfakih/code/IsaacBenlfakih/MajorilleGarden/app/assets/images/p5.jpg"), filename: "p5.jpg", content_type: "image/png")
+product.save
+
+product = Product.create(name: "Majorille Garden Zeep", description: "This is body cream", price: 11.30)
+product.photo.attach(io: File.open("/home/isaacbenlfakih/code/IsaacBenlfakih/MajorilleGarden/app/assets/images/p1.jpg"), filename: "p1.jpg", content_type: "image/png")
+product.save
+
+product = Product.create(name: "Body Conditioner", description: "This is body cream", price: 11.30)
+product.photo.attach(io: File.open("/home/isaacbenlfakih/code/IsaacBenlfakih/MajorilleGarden/app/assets/images/p2.jpg"), filename: "p2.jpg", content_type: "image/png")
+product.save
+
+product = Product.create(name: "Majorille Garden Blue Shit", description: "This is body cream", price: 11.30)
+product.photo.attach(io: File.open("/home/isaacbenlfakih/code/IsaacBenlfakih/MajorilleGarden/app/assets/images/p6.jpg"), filename: "p6.jpg", content_type: "image/png")
+product.save
+
+product = Product.create(name: "Body Cream", description: "This is body cream", price: 11.30)
+product.photo.attach(io: File.open("/home/isaacbenlfakih/code/IsaacBenlfakih/MajorilleGarden/app/assets/images/P3.jpg"), filename: "P3.jpg", content_type: "image/png")
+product.save
+
+
+
+# article = Article.new(title: "NES", body: "A great console")
+# article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+# article.save
