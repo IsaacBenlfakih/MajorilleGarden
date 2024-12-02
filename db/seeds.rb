fca1a6be094b04db383412be70d151d7a6d08213
isaac benlfakih <isaacbenlfakih@gmail.com>
2024-12-02 15:45:24 +0100

    fuck

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,22 @@ require "open-uri"
 file = URI.parse("https://res.cloudinary.com/dyxnew8rp/image/upload/v1732961527/p3.jpg").open
 product = Product.create(name: "Majorille Arganolie, 100mL", description: "De vruchten van de arganboom worden met hand geoogst door een vrouwencoöperatie in Marokko, waarna de voedende olie zorgvuldig uit de pitten wordt geperst", price: 24.95)
 product.photo.attach(io: file, filename: "p3.jpg", content_type: "image/png")
-product.save
+product.save!
 
 file = URI.parse("https://res.cloudinary.com/dyxnew8rp/image/upload/v1732961527/p5.jpg").open
 product = Product.create(name: "Majorille Arganolie, 50mL", description: "De vruchten van de arganboom worden met hand geoogst door een vrouwencoöperatie in Marokko, waarna de voedende olie zorgvuldig uit de pitten wordt geperst", price: 12.99)
 product.photo.attach(io: file, filename: "p5.jpg", content_type: "image/png")
-product.save
+product.save!
 
 file = URI.parse("https://res.cloudinary.com/dyxnew8rp/image/upload/v1732961527/p1.jpg").open
 product = Product.create(name: "Majorille Zeep", description: "Deze verfijnde gourmetzeep bevat arganolie, zorgvuldig gewonnen door een vrouwencoöperatie in Marokko. De kostbare olie zorgt voor een zijdezacht en luxueus schuim, dat pure verwennerij biedt bij elk gebruik.", price: 4.99)
 product.photo.attach(io: file, filename: "p1.jpg", content_type: "image/png")
-product.save
+product.save!
 
 file = URI.parse("https://res.cloudinary.com/dyxnew8rp/image/upload/v1732961527/p2.jpg").open
 product = Product.create(name: "Majorille Body Conditioner", description: "In deze verfijnde bodyconditioner zijn delicate rozenabsolue en Marokkaanse arganolie zorgvuldig vermengd met biologische kruiden.", price: 3.95)
 product.photo.attach(io: file, filename: "p2.jpg", content_type: "image/png")
-product.save
+product.save!
 
 
 
