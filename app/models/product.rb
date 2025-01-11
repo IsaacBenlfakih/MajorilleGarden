05f8d9c186afe5ccc3fdfad519e7056d5403637e
isaac benlfakih <isaacbenlfakih@gmail.com>
2025-01-11 17:49:26 +0100

    fixed database

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :product_orders
   has_many :orders, through: :product_orders
+  has_many :orderables
+  has_many :carts, through: :orderables
   has_one_attached :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+   get 'cart', to: 'cart#show'
+   post 'cart/add'
+   post 'cart/remove'
    get '/about', to: 'pages#aboutus'
    get '/zandbad', to: 'pages#zandbad'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
